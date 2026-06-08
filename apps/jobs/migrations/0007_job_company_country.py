560f3a32fb4d3adaeb1bf9232d2924ae3a38dabc
Rehd <michaeloseromi@gmail.com>
2026-06-08 20:07:33 +0100

    Fix company_country length

--- apps/jobs/migrations/0007_job_company_country.py
+++ apps/jobs/migrations/0007_job_company_country.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='job',
             name='company_country',
-            field=django_countries.fields.CountryField(blank=True, max_length=2, null=True),
+            field=django_countries.fields.CountryField(blank=True, max_length=100, null=True),
         ),
     ]
